91b3fe6c7d2038e2a0a9b36bc9a520c9e4b1743d
Wambui-N <wambuin18@gmail.com>
2023-07-14 15:20:39 +0300

    ignore errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const { types } = require('util')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
         domains: ['cdn.imagin.studio']
+    },
+    typescript: {
+        ignoreBuildErrors: true,
     }
 }
 
